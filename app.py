010af31d185d4533b542dd59350e40b568361a3a
Brandon Boatright <bboatright013@gmail.com>
2020-06-24 17:06:08 -0400

    first commit, boggle app legs added

---
+++ app.py
@@ -0,0 +1,13 @@
+from boggle import Boggle
+from flask import Flask, render_template, request, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+
+app.debug = True
+
+debug = DebugToolbarExtension(app)
+
+
+boggle_game = Boggle()
